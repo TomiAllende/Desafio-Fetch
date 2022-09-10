d6509a49c554629e29c215f62a8879192dcbbabe
TomiAllende <tomasallende2001@gmail.com>
2022-09-10 14:20:28 -0300

    realizo entrega final

--- js/carrito.js
+++ js/carrito.js
@@ -1,5 +1,19 @@
 // En funcion a los pedidos que haya en e carro los sumo para obtener el total.
 
+//Los num son los prodcutos
+
+function sumar(num1, num2, num3) {
+    return num1+num2+num3
+}
+
+function calculos(operacion, num1, num2, num3) {
+let resultado = operacion(num1, num2, num3)
+console.log(resultado)
+return resultado
+}
+
+// En funcion a los pedidos que haya en e carro los sumo para obtener el total.
+
 const { $dataMetaSchema } = require("ajv")
 
 //Los num son los prodcutos
